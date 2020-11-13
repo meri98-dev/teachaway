9fe5e26f5d92e9c0980ceeb32bbaa7035d2a1b68
Meri Roshi <meriroshi90@gmail.com>
2020-11-13 14:52:01 +0100

    Building courses section

--- wp-content/themes/understrap/page-templates/tefl-template.php
+++ wp-content/themes/understrap/page-templates/tefl-template.php
@@ -113,7 +113,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 				</div>
 				<div class="row">
 					<div class="col-12">
-						
+
 						<div class="row images">
 							<?php while(have_rows('employers_repeater')): the_row();
 								$image = get_sub_field('image'); ?>
@@ -122,14 +122,58 @@ $container = get_theme_mod( 'understrap_container_type' );
 								</div>
 							<?php endwhile;?>
 						</div>
-						
+
 					</div>
 				</div>
 			</section>
 
-		</div><!-- #fullpage-->
+			<div class="courses container-fluid px-0">
+				<div class="row">
+					<div class="col-12">
+						<div class="section-title">
+							<h1><?php the_field('courses_tefl');?></h1>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+		     		<div class="col-12">
+						
+					<?php 
+						$count =0;
+						while(have_rows('courses_repeater')):the_row(); ?>
+							<div class="row <?php	if( $count % 2 == 1) { echo "right-side";} ?> mx-0"> 
+								<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 px-0">
+									<?php $main_image = get_sub_field('main_image'); ?>
+									<div class="main-image">
+									<img class = "img-fluid w-100" src="<?php echo $main_image['url']; ?>" alt="">
+								<!-- <div class="main-image" style="background-image: url('<?php echo $main_image['url'];?>')";></div> -->
+								</div>
+								</div>
+								<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 second-col">
+									<?php $uni_image = get_sub_field('university_image'); ?>
+									<img class = "img-fluid uni-image"src="<?php echo $uni_image['url']; ?>" alt="">
+									<div class="text-repeater">
+										<?php while(have_rows('text_repeater')):the_row(); ?>
+											<p><i class = "fa fa-check"></i><?php the_sub_field('text');?></p>
+										<?php endwhile; ?>
+									</div>
+									<div class="buttons">
+										<a class="cta-button cta-green" href="<?php echo $button_1['cta_link']  ?>"><?php echo $button_1['cta_text']  ?></a>
+										<a class="cta-button cta-white" href="<?php echo $button_2['cta_link']  ?>"><?php echo $button_2['cta_text']  ?></a>
+									</div>
+								</div>
+							</div>
+							<?php
+							$count++;
+						endwhile; ?>
+					</div>
+				</div>
+			</div>
+		</section>
 
-	</div><!-- .row end -->
+	</div><!-- #fullpage-->
+
+</div><!-- .row end -->
 
 </div><!-- .container-fluid -->
 
