ff2720a978234ec722a3e2d59d19f96afdd3149f
Meri Roshi <meriroshi90@gmail.com>
2020-11-16 22:01:54 +0100

    Building Footer - Done:

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -27,38 +27,43 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="row top-footer">
 						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
-							<h5><?php the_field('latest_post_text','options'); ?></h5>
+							<div class="col-12">
+								<h5><?php the_field('latest_post_text','options'); ?></h5>
+							</div>		
+							<div class="col-12">
+								<div class="slick-slider">
+									<?php
+									$args = array(
+										'post_type' => 'post'
+									);
+									$post_query = new WP_Query($args);
 
-							<div class="slick-slider">
-								
-								<?php
-								$args = array(
-									'post_type' => 'post'
-								);
+									if($post_query->have_posts() ) {
+										while($post_query->have_posts() ) {
+											$post_query->the_post();
+											?>
+											<div class="slick-item">
+												<p><?php the_title(); ?></p>
+											</div>
 
-								$post_query = new WP_Query($args);
-
-								if($post_query->have_posts() ) {
-									while($post_query->have_posts() ) {
-										$post_query->the_post();
-										?>
-										<div class="slick-item">
-											<h5><?php the_title(); ?></h5>
-										</div>
-
-										<?php
+											<?php
+										}
 									}
-								}
-								?>
+									?>
+								</div>
 							</div>
-
 						</div>
 						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
-							<h5><?php the_field('about_menu_name','options'); ?></h5>
-
-							<div class="footer-menu">
-								<?php wp_nav_menu( array( 'theme_location' => 'footer_menu') ); ?>
+							<div class="col-12">
+								<h5><?php the_field('about_menu_name','options'); ?></h5>
 							</div>
+							
+							<div class="col-12">
+								<div class="footer-menu">
+									<?php wp_nav_menu( array( 'theme_location' => 'footer_menu') ); ?>
+								</div>
+							</div>
+
 						</div>
 						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
 							<div class="col-12">
@@ -66,7 +71,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 									<h5><?php the_field('get_in_touch_text','options'); ?></h5>
 									<p><?php the_field('location','options'); ?></p>
 								</div>
-
 							</div>
 							<div class="col-12">
 								<h5><?php the_field('also_seen_on_text','options'); ?></h5>
@@ -82,27 +86,39 @@ $container = get_theme_mod( 'understrap_container_type' );
 								</div>
 							</div>
 						</div>
-					<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
-
-						<div class="col-12">
-							<h5><?php the_field('contacts_text','options'); ?></h5>
+						<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-xs-12">
+							<div class="col-12">
+								<h5><?php the_field('contacts_text','options'); ?></h5>
+							</div>
+							<div class="col-12">
+								<ul class="locations">
+									<?php while(have_rows('contacts','options')):the_row(); ?>
+										<li><a href="<?php the_sub_field('link');?>"><?php the_sub_field('location');?></a></li>
+									<?php endwhile; ?>
+								</ul>
+							</div>
 						</div>
-						<?php while(have_rows('contacts','options')):the_row(); ?>
-							<ul class="locations">
-								<li><a href="<?php the_sub_field('link');?>"><?php the_sub_field('location');?></a></li>
+					</div> <!-- .top-footer -->
 
-							</ul>
-						<?php endwhile; ?>
+					<div class="row bottom-row">
+						<div class="col-xl-2 col-lg-2 col-md-2 col-sm-12 col-xs-12">
+							<?php $main_logo = get_field('footer_main_logo','options'); ?>
+							<img src="<?php echo $main_logo['url']; ?>" alt="" class="footer-main-logo">
+							<p class = "copyright-text"><?php the_field('copyright_text','options'); ?></p>
+						</div>
+						<div class="col-xl-10 col-lg-10 col-md-10 col-sm-12 col-xs-12">
+							<?php $second_logo = get_field('footer_second_logo','options'); ?>
+							<img src="<?php echo $second_logo['url']; ?>" alt="" class="footer-second-logo">
+						</div>
 					</div>
-		</div>
 
-	</footer><!-- #colophon -->
+				</footer><!-- #colophon -->
 
-</div><!--col end -->
+			</div><!--col end -->
 
-</div><!-- row end -->
+		</div><!-- row end -->
 
-</div><!-- container end -->
+	</div><!-- container end -->
 
 </div><!-- wrapper end -->
 
